e60ba128bb87412d9ae13665f59eb8fddbe1fbe6
LucaJahnen <luca.jahnen@gmail.com>
2024-11-17 14:13:30 +0100

    improving performance and seo

--- src/main.tsx
+++ src/main.tsx
@@ -1,60 +1,10 @@
 import { createRoot } from 'react-dom/client'
-import Error from './pages/error'
-import App from './App.tsx'
-import IntervalQuiz from './pages/interval-quiz'
-import Metronome from './pages/metronome'
-import Dictionary from './pages/dictionary'
-import BuildingIntervals from './pages/building-intervals'
-import ChordQuiz from './pages/chord-quiz/index.tsx'
-import BuildingChords from './pages/building-chords/index.tsx'
-import PrivacyPolicy from './pages/privacy-policy/index.tsx'
-import TermsOfService from './pages/terms-of-service/index.tsx'
+import { Suspense } from "react"
 import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom"
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <Error />
-  },
-  {
-    path: "/interval-quiz",
-    element: <IntervalQuiz />,
-  },
-  {
-    path: "/metronome",
-    element: <Metronome />,
-  },
-  {
-    path: "/dictionary",
-    element: <Dictionary />,
-  },
-  {
-    path: "/building-intervals",
-    element: <BuildingIntervals />,
-  },
-  {
-    path: "/chord-quiz",
-    element: <ChordQuiz />,
-  },
-  {
-    path: "/building-chords",
-    element: <BuildingChords />,
-  },
-  {
-    path: "/privacy-policy",
-    element: <PrivacyPolicy />,
-  },
-  {
-    path: "/terms-of-service",
-    element: <TermsOfService />,
-  },
-]);
+import Root from './root.js'
 
 createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />
+  <Suspense fallback={<p>Loading...</p>}>
+    <Root />
+  </Suspense>
 )
---
+++ src/root.tsx
@@ -0,0 +1,64 @@
+import { Suspense, lazy } from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const App = lazy(() => import('./App'))
+const Error = lazy(() => import('./pages/error'))
+const IntervalQuiz = lazy(() => import('./pages/interval-quiz'))
+const Metronome = lazy(() => import('./pages/metronome'))
+const Dictionary = lazy(() => import('./pages/dictionary'))
+const BuildingIntervals = lazy(() => import('./pages/building-intervals'))
+const ChordQuiz = lazy(() => import('./pages/chord-quiz'))
+const BuildingChords = lazy(() => import('./pages/building-chords'))
+const PrivacyPolicy = lazy(() => import('./pages/privacy-policy'))
+const TermsOfService = lazy(() => import('./pages/terms-of-service'))
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <Suspense fallback={<p>Loading...</p>}>
+            <App />
+        </Suspense>
+      ),
+      errorElement: (
+        <Suspense fallback={<p>Loading...</p>}>
+          <Error />
+        </Suspense>
+      )
+    },
+    {
+      path: "/interval-quiz",
+      element: <IntervalQuiz />,
+    },
+    {
+      path: "/metronome",
+      element: <Metronome />,
+    },
+    {
+      path: "/dictionary",
+      element: <Dictionary />,
+    },
+    {
+      path: "/building-intervals",
+      element: <BuildingIntervals />,
+    },
+    {
+      path: "/chord-quiz",
+      element: <ChordQuiz />,
+    },
+    {
+      path: "/building-chords",
+      element: <BuildingChords />,
+    },
+    {
+      path: "/privacy-policy",
+      element: <PrivacyPolicy />,
+    },
+    {
+      path: "/terms-of-service",
+      element: <TermsOfService />,
+    },
+])
+
+const Root = () => <RouterProvider router={router} />
+export default Root
