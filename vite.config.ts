337ac30207fb7c94808cab8e1e2f48c5082373b5
LucaJahnen <luca.jahnen@gmail.com>
2025-07-25 12:17:17 +0200

    implementing preact for performance

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import path from "path"
-import react from "@vitejs/plugin-react"
+// import react from "@vitejs/plugin-react"
+import preact from '@preact/preset-vite'
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [preact()],
+  build: {
+    sourcemap: true,
+      minify: 'esbuild',
+      rollupOptions: {
+        treeshake: true,
+      },
+  },
   assetsInclude: ['**/*.riv'],
   resolve: {
     alias: {
